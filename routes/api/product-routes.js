25caa726459340a44339df64e5552fd5ec72ae44
DylanCas <djcasabona12@gmail.com>
2023-02-23 00:22:28 -0700

    Removed commented code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,13 +4,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-// router.get('/', (req, res) => {
-//   // find all products
-//   // be sure to include its associated Category and Tag data
-//   Product.findAll().then((productData) => {
-//     res.json(productData);
-//   });
-// });
 router.get('/', (req, res) => {
   // find all products, including associated Category and Tag data
   Product.findAll({
